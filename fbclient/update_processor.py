77c57ebfa2fc37899c8e0b0c9f0fefeceba068c9
ds <sun.dian0720@gmail.com>
2023-04-17 11:35:20 +0200

    v1.1.0: update bootstrapping

--- fbclient/update_processor.py
+++ fbclient/update_processor.py
@@ -1,15 +1,18 @@
 from threading import Event
 from fbclient.config import Config
 from fbclient.interfaces import DataUpdateStatusProvider, UpdateProcessor
+from fbclient.status_types import State
 
 
 class NullUpdateProcessor(UpdateProcessor):
 
     def __init__(self, config: Config, dataUpdateStatusProvider: DataUpdateStatusProvider, ready: Event):
         self.__ready = ready
+        self.__store = dataUpdateStatusProvider
 
     def start(self):
         self.__ready.set()
+        self.__store.update_state(State.ok_state())
 
     def stop(self):
         pass
